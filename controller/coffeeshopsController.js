bc2c68138d130dd60185fe19c8c52f0535cde344
BreffJaun <braun_jeff@web.de>
2023-01-17 10:40:40 +0100

    Habe Gott gespielt

--- controller/coffeeshopsController.js
+++ controller/coffeeshopsController.js
@@ -29,9 +29,8 @@ const getCoffeeshops = async (req, res, next) => {
 const getCoffeeshop = async (req, res, next) => {
   try {
     const shopId = req.params.id
-    const coffeeshop = await CoffeeshopModel.find({name:shopId});
-    console.log(shopId);
-    console.log(coffeeshop);
+    const coffeeshop = await CoffeeshopModel.findById(shopId);
+
     res.status(200).json(coffeeshop);
   } catch (error) {
     next(error);
